726bd67bdc7dbdb54ead155ccfedd14e9fbe82bd
Dzakiramokoginta <89532735+Dzakiramokoginta@users.noreply.github.com>
2021-08-25 23:04:15 +0800

    Add files via upload

---
+++ Tugas.m
@@ -0,0 +1,50 @@
+% Nama : Dzakirah Raihana Mokoginta 
+% NIM  : 200209500031
+% Kelas: PTIK B
+
+% nomor 1
+12/3.5
+(3+5/4)^2
+(0.25^2+0.75^2)^1/2
+2/(6/0.3)
+
+% nomor 2
+A = 25;B = 50; C = 125; D = 89;
+X = A+B+C
+Y = A/(D+B)
+Z = D^(A/B)+C
+
+
+% nomor 3
+Luas = 9
+Kel_1 = 9
+2_data = 9 
+Diff:3 = 9 
+Time = 9
+Time_from_start = 9
+10_hasil_terakhir = 9 
+Nilai-awal = 9 
+
+% nomor 4
+x = pi/6; y = 0.001;
+sqrt(y)
+exp(-x)
+sin(x)
+cos(2*x)
+tan(3*x)
+log10(y)
+log2(y)
+log(y)
+
+% nomor 5
+p = 9 + 16i; q = -9 + 16i;
+r = p*q
+s = p/q
+p-r
+r+s
+p^2
+sqrt(q)
+abs(p), angle(p)
+abs(q), angle(q)
+abs(r), angle(r)
+abs(s), angle(s)
